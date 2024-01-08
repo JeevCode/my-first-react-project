6ff20a45f2240e8825b787849ac0f51082e7ec62
Jeeva Subramanian <jeeva.subramanian@talentoz.com>
2024-01-08 21:48:13 +0530

    API integerated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import mainStyles from "../assets/css/Main.module.css";
 import Footer from "../components/layout/Footer";
 import Navbar from "../components/layout/Navbar";
 
 function Home() {
+  const baseUrl = "https://official-joke-api.appspot.com/random_joke";
+  // const baseUrl = process.env.API_BASE
+  const [first, setfirst] = useState("");
+  const apiFunction = async () => {
+    try {
+      const apiResponse = await fetch(baseUrl, { method: "GET" });
+      if (!apiResponse.ok) {
+        throw new Error(`HTTP error! Status: ${apiResponse.status}`);
+      }
+  
+      const API = await apiResponse.json();
+      setfirst(API);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+
   return (
     <>
       <Navbar />
@@ -14,6 +32,10 @@ function Home() {
             This is the home page of my website. Here you can find information
             about me and my work.
           </p>
+          <p>  {first.id} {first.punchline}</p>
+          <p>   </p>
+          <p>  {first.setup} </p>
+          <button onClick={apiFunction}>Get Joke Here</button>
         </div>
       </div>
       <Footer />
